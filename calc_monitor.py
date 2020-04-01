0c585d02b0439044a9ed547d9ed95b534e6e510d
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-04-01 17:29:25 +0200

    Write logs in phon dir

--- calc_monitor.py
+++ calc_monitor.py
@@ -71,10 +71,10 @@ def run_alamode(d='phon', prefix='cryst', kpath='cryst', dfset='DFSET', sc='../s
 
     for p, l in zip((fit, phon, alm, anph), ('fit', 'phon', 'alm', 'anphon')):
         if p.stdout is not None:
-            with open(f'{prefix}_{l}.log', 'wt') as lf:
+            with open(f'{d}/{prefix}_{l}.log', 'wt') as lf:
                 lf.write(p.stdout.decode())
         if p.stderr is not None:
-            with open(f'{prefix}_{l}.err', 'wt') as lf:
+            with open(f'{d}/{prefix}_{l}.err', 'wt') as lf:
                 lf.write(p.stderr.decode())
 
     return all([r.returncode==0 for r in  (fit, phon, alm, anph)]), fit, phon, alm, anph
