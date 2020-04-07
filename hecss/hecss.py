bdc02fa2269d7a7027fd8b7105f4066fb27f0111
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-04-07 10:18:31 +0200

    Wider prior distribution. Correct too sharp energy distro.

--- hecss/hecss.py
+++ hecss/hecss.py
@@ -196,7 +196,7 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, directory=No
         alpha *= exp(q_x + q_star_x_star - q_star_x - q_x_star)
 
         w_prev = w
-        w *= 1-2*(expit((e_star-E_goal)/Es)-0.5)*delta*(10 if i==0 else 1)
+        w *= 1-2*(expit((e_star-E_goal)/Es/2)-0.5)*delta*(10 if i==0 else 1)
 
         if np.random.rand() < alpha:
             x = x_star
