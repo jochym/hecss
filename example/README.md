b37dc71c56125844777d2b5abfa4e8a35e84e396
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-07-16 12:45:25 +0200

    Add monitoring notebooks

--- example/README.md
+++ example/README.md
@@ -1,18 +1,20 @@
-# Example directory
+# HECSS Examples
 
 This is a simple example of 3C-SiC crystal to use for testing.
 This directory contains all files required to test and learn HECSS.
 
 ## Directories:
 
+### VASP_3C-SiC
+
+This directory is for learning/testing with VASP, which is more complicated and requires non-free VASP calculator and proper setup.
 - sc_1x1x1 - crystalographic unit cell for quick tests
 - sc_2x2x2 - 2x2x2 supercell for better quality, but slower, tests
 - phon - directory for phonon calculations (contains path and born files)
 
-## How to use
+#### Usage
 
-1. Copy one of sc directories (sc_1x1x1 recommended for starters) to sc directory 
-*above* the main hecss directory (i.e. two directories up).
+1. Copy one of sc directories (sc_1x1x1 recommended for starters) to sc directory *above* the main hecss directory (i.e. two directories up).
 2. Copy the phon directory to the same place (i.e. *above* the main hecss directory).
 3. Open run_sampler.py as notebook in jupyterlab and set `base_dir` to `..`.
 4. Set `T_goal` to desired temperature
@@ -20,4 +22,16 @@ This directory contains all files required to test and learn HECSS.
 5. Run the notebook
 6. You can monitor calculations by opening `monitor_stats.py` and `monitor_phonons.py` 
    scripts in jupyterlab as notebooks
+
+
+### LAMMPS_3C-SiC
+
+This directory uses ASAP3/LAMMPS effective potential codes as a forces calculator.
+Use of this directory requires no non-free components. The results are less accurate but you can calculate much larger systems and do it much faster. ASAP3 can be installed from conda or pypi:
+```
+conda install -c jochym asap3
+```
+
+#### Usage
+
 
--- hecss/core.py
+++ hecss/core.py
@@ -58,7 +58,7 @@ def normalize_conf(c, base):
 
 # Cell
 
-def write_dfset(fn, c, n=0):
+def write_dfset(fn, c):
     '''
     Append displacement-force data from the conf to the fn file.
     The format is suitable for use as ALAMODE DFSET file.
@@ -66,7 +66,7 @@ def write_dfset(fn, c, n=0):
     File need not exist prior to first call.
     If it does not it will be created.
     '''
-    i, x, f, e = c
+    n, i, x, f, e = c
     with open(fn, 'at') as dfset:
         print(f'# set: {n:04d} config: {i:04d}  energy: {e:8e} eV/at', file=dfset)
         for ui, fi in zip(x,f):
@@ -77,8 +77,10 @@ def write_dfset(fn, c, n=0):
 
 # Cell
 
-def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
-            directory=None, reuse_base=None, verb=True):
+def HECSS(cryst, calc, T_goal, width=1, maxburn=20,
+            N=None, w_search = True, delta_sample=0.01,
+            directory=None, reuse_base=None, verb=True,
+            priors=None, posts=None, width_list=None):
     '''
     Run HECS sampler on the system `cryst` using calculator `calc` at target
     temperature `T_goal`. The `delta`, `width`, `maxburn` and `directory` parameters
@@ -100,37 +102,41 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
     sampler.close()
     ```
 
-    INPUT
-    =====
-    cryst   - ASE structure to sample
-    calc    - ASE calculator to use for potential energy evaluations
-    T_goal  - Target temperature in Kelvin
+    ### INPUT
 
-    delta   - speed of the adaptation - maximal change of the prior width in one step
-    width   - initial width of the position distribution in Angstrom
-    maxburn - max number of burn-in steps
-    sigma   - width in energy sigmas of the prior energy distribution.
-              This should be approx 2 (default), if your posteriors seem too narrow
-              make it bigger, if too wide make it smaller (1-3 range in general).
-              Too large a number makes the acceptance ratio lower.
+    cryst        - ASE structure to sample
+    calc         - ASE calculator to use for potential energy evaluations
+    T_goal       - Target temperature in Kelvin
+    width        - initial width of the position distribution,
+                   relative to the heurestic value defined inside function
+    maxburn      - max number of burn-in steps
+    N            - Number of iterations. If None (default) the generator will never terminate.
+    w_search     - Run search for initial w. If false start from whatever is passed as width.
+    delta_sample - Prior width adaptation rate. The default is sufficient in most cases.
 
-    directory - directory for calculations and generated samples. If left as None,
-                the `calc/{T_goal:.1f}K/` will be used and the generated samples will be
-                stored in the `smpl/{i:04d}` subdirectories.
+    directory    - (only for VASP calculator) directory for calculations and generated samples .
+                   If left as None, the `calc/{T_goal:.1f}K/` will be used and the generated
+                   samples will be stored in the `smpl/{i:04d}` subdirectories.
 
-    reuse_base - None or the base calc directory for the ground state config
-                 if None the base will be recalculated at the start of the run
-                 if directory name - the energy from this dir will be reused as
-                 ground state energy for the calculation.
-                 Be careful to have the same VASP setup in calc and reuse_base,
-                 otherwise the ground state energy and distribution wil be wrong.
+    reuse_base   - (only for VASP calculator) None or the base calc directory for the ground
+                   state config. If None the base will be recalculated at the start of the run.
+                   If directory name - the energy from this dir will be reused as ground state
+                   energy for the calculation. Be careful to have the same VASP setup in calc
+                   and reuse_base, otherwise the ground state energy and distribution will be wrong.
 
-    verb    - print verbose progress messages for interactive use
+    verb         - print verbose progress messages for interactive use
 
-    OUTPUT
-    ======
+    #### Output parameters
+
+    priors       - Output parameter. If not None, store in passed list the sequence of priors.
+    posts        - Output parameter. If not None, store in passed list the sequence of posteriors.
+    width_list   - Output parameter. If not None, store in passed list the sequence of widths.
+
+    ### OUTPUT
+
     The generator yields samples from the thermodynamic distribution at T=T_goal as tuples
-    (index, displacement, forces, energy):
+    (number, index, displacement, forces, energy):
+    number       - sample number, always increasing
     index        - integer numbering the samples in the `smpl` subdirectory. The index may
                    repeat if the sample must be repeated in the sequence.
     displacement - set of atomic displacements (in A) in the sample (numpy array)
@@ -141,9 +147,9 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
 
     def smpl_print(r=0):
         if i==0:
-            print(f'Burn-in sample:{k}  w:{w:.4f}  alpha:{alpha:6.4e}  dE:{(e_star-E_goal)/(2*Es):+6.2f} sigma', end='\r')
+            print(f'Burn-in sample:{k}  w:{w:.4f}  alpha:{alpha:6.4e}  dE:{(e_star-E_goal)/Es:+6.2f} sigma', end='\n')
         else :
-            print(f'Sample:{i-1:04d}  a:{100*i/n:5.1f}%  w:{w:.4f}  <w>:{np.mean(wl):.4f}  alpha:{alpha:6g} ' + (r*'x'), end='\r')
+            print(f'Sample:{n:04d}  a:{100*i/n:5.1f}%  w:{w:.4f}  <w>:{np.mean([_[0] for _ in wl]):.4f}  alpha:{alpha:10.3e} ' + (r*'x'), end='\r')
         sys.stdout.flush()
 
 
@@ -166,10 +172,24 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
     P = stats.norm.pdf
     Q = stats.norm
 
+    # This comes from the fitting to 3C-SiC case
+    w_scale = 1.667e-3 * (T_goal**0.5) #(T_goal**0.47)
+
     w = width
     w_prev = w
-    x = Q.rvs(size=dim, scale=w)
-    wl = []
+
+    x = Q.rvs(size=dim, scale=w * w_scale  )
+
+    if width_list is None :
+        wl = []
+    else :
+        wl = width_list
+
+    if priors is None:
+        priors = []
+
+    if posts is None:
+        posts = []
 
     i = 0
     n = 0
@@ -189,7 +209,10 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
         sys.stdout.flush()
 
     cr.set_positions(cryst.get_positions()+x)
-    cr.calc.set(directory=f'{basedir}/smpl/{i:04d}')
+    try :
+        cr.calc.set(directory=f'{basedir}/smpl/{i:04d}')
+    except AttributeError :
+        pass
 
     e = (cr.get_potential_energy()-Ep0)/nat
     f = cr.get_forces()
@@ -198,63 +221,87 @@ def HECSS(cryst, calc, T_goal, delta=0.05, width=0.033, maxburn=20, sigma=2,
     r = 0
     alpha = 0
 
-
     if verb:
         print(f'Starting burn-in.            ', end='\r')
         sys.stdout.flush()
 
-    while True:
+    while N is None or n <= N:
         if verb and (n>0 or k>0):
             smpl_print(r)
 
-        x_star = Q.rvs(size=dim, scale=w)
+        x_star = Q.rvs(size=dim, scale=w * w_scale )
 
         cr.set_positions(cryst.get_positions()+x_star)
-        cr.calc.set(directory=f'{basedir}/smpl/{i:04d}')
+        try :
+            cr.calc.set(directory=f'{basedir}/smpl/{i:04d}')
+        except AttributeError :
+            pass
 
         e_star = (cr.get_potential_energy()-Ep0)/nat
         f_star = cr.get_forces()
 
-        alpha = P(e_star, E_goal, Es) / P(e, E_goal, Es)
+        wl.append((w,e_star))
 
-        q_x = log(Q.pdf(x, w_prev)).sum()
-        q_star_x_star = log(Q.pdf(x_star, w)).sum()
-        q_star_x = log(Q.pdf(x, w)).sum()
-        q_x_star = log(Q.pdf(x_star, w_prev)).sum()
-
-        alpha *= exp(q_x + q_star_x_star - q_star_x - q_x_star)
-
+        if i==0 :
+            delta = 0.05
+        else :
+            delta = delta_sample
         w_prev = w
-        w *= 1-2*(expit((e_star-E_goal)/Es/sigma)-0.5)*delta*(10 if i==0 else 1)
+
+        if w_search :
+            w = w*(1-2*delta*(expit((e_star-E_goal)/Es/3)-0.5))
+            if i==0 and abs(e_star-E_goal) > Es :
+                # We are in w-search mode but still far from E_goal
+                # Continue
+                k += 1
+                if k>maxburn :
+                    print(f'\nError: reached maxburn ({maxburn}) without finding target energy.\n'+
+                        f'You probably need to change initial width parameter (current:{w}) to a {"higher" if (e_star-E_goal)<0 else "lower"} value.')
+                    return
+                # Continue searching for proper w
+                continue
+
+        if i==0 :
+            # We are in w-search mode and just found a proper w
+            # switch to sampling mode by making sure the sample is accepted
+            # 2 is larger than any result of np.random.rand()
+            alpha = 2
+            # clean up the w table
+            wl.clear()
+            prior_len=1
+        else :
+            # Sampling mode
+            alpha = P(e_star, E_goal, Es) / P(e, E_goal, Es)
+
+            priors.append((n, i-1, x_star, f_star, e_star))
+
+            if len(priors) > 3 :
+                # There is no sense in fitting priors to normal dist if we have just 2-3 samples
+                # The 4 samples is still rather low but seems to work well enough
+                if  len(priors) > 1.1*prior_len:
+                    # Re-fit the prior only if we get 10% more samples
+                    pfit = stats.norm.fit([_[-1] for _ in priors])
+                    prior_len = len(priors)
+
+                # Take into account estimated transition probability
+                alpha *= stats.norm.pdf(e, *pfit)/stats.norm.pdf(e_star, *pfit)
 
         if np.random.rand() < alpha:
             x = x_star
             e = e_star
             f = f_star
-            if i==0 and abs(e_star-E_goal) > 2*Es :
-                # At the burn-in stage and still further then 2 sigma from target
-                # Let us keep searching for correct w
-                k += 1
-                if k>maxburn :
-                    print(f'\nError: reached maxburn ({maxburn}) without finding target energy.\n'+
-                      f'You probably need to change initial width parameter to a {"higher" if (e_star-E_goal)<0 else "lower"} value.')
-                    return
-                continue
-            else :
-                # We are at sampling stage or just found the proper w
-                # Either way: switch to the next sample directory (i)
-                # and zero the rejection counter
-                wl.append(w)
-                i += 1
-                n += 1
-                r = 0
+            i += 1
+            r = 0
         else:
-            # Sample rejected - nothing to yield. Try again
+            # Sample rejected - stay put
             r += 1
-            n += 1
-            continue
+
+        n += 1
 
         if verb:
             smpl_print(r)
 
-        yield i-1, x, f, e
+        if posts is not None :
+            posts.append((n, i-1, x, f, e))
+
+        yield n, i-1, x, f, e
