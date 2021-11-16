e5c9185b266f8a81f9f6ff2c147bb43bb96abdcd
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2021-11-16 09:45:41 +0100

    Add alternative interface, tune options

--- hecss/cli.py
+++ hecss/cli.py
@@ -5,6 +5,7 @@ __all__ = ['dfset_writer', 'hecss_sampler']
 # Cell
 # export
 import click
+from fastcore.script import Param, call_parse, bool_arg, store_true, store_false
 from pathlib import Path
 import os
 import ase
@@ -23,12 +24,32 @@ def dfset_writer(s, sl, workdir=''):
     return False
 
 # Cell
-def hecss_sampler(fname='CONTCAR', workdir='WORK', label='hecss',
-                  T=300, calc='VASP', dfset=True, N=10, command='./run-calc'):
+@call_parse
+def hecss_sampler(fname:  Param("Supercell structure file."
+                                "The containing directory must be readable by Vasp(restart)."
+                                , str),
+                  workdir:Param("Output directory for samples", str)='calc',
+                  label:  Param("Job label passed to run-calc command", str)='hecss',
+                  T:      Param("Target temperature (K)", float)=300,
+                  width:  Param("Initial scale of the prior distribution", float)=1.0,
+                  calc:   Param("Type of the calculator. Only VASP supported.", str)='VASP',
+                  dfset:  Param("Write DFset file", store_false)=True,
+                  N:      Param("Number of samples", int)=10,
+                  cmd:    Param("Command for job submission", str)='./run-calc',
+                  V:      Param("Print version and exit", store_true)=False):
     '''
-    Run HECSS sampler on the structure in the directory.
-    fname - Basic structure file. Any type recognized by ASE is accepted.
+    Run HECSS sampler on the structure passed in fname.
     '''
+
+    if V:
+        import hecss
+        print(f'HECSS ver. {hecss.__version__}\n'
+               'High Efficiency Configuration Space Sampler\n'
+               '(C) 2021 by Paweł T. Jochym\n'
+               '    License: GPL v3 or later')
+        return
+
+
     print(f'Running HECSS on {fname}\n',
           f'Temperature:    {T}K\n',
           f'Work directory: {workdir}\n',
@@ -43,36 +64,42 @@ def hecss_sampler(fname='CONTCAR', workdir='WORK', label='hecss',
         cryst = ase.Atoms(calculator.atoms)
         cryst.set_calculator(calculator)
         calculator.set(directory=workdir)
-        command = Path(command)
+        command = Path(cmd)
         calculator.set(command=f'{command.absolute()} {label}')
+    else:
+        print(f'The {calc} calculator is not supported.')
+        return
     sentinel = None
     if dfset :
         sentinel = dfset_writer
-    sampler = HECSS(cryst, calculator, T, directory=workdir)
+    sampler = HECSS(cryst, calculator, T, directory=workdir, width=width)
     samples = sampler.generate(N, sentinel=sentinel, workdir=workdir)
     return samples
 
 # Cell
 @click.command()
-@click.argument('fname', default='CONTCAR', type=click.Path(exists=True))
-@click.option('-w', '--workdir', default="WORK", type=click.Path(exists=True), help="Work directory")
+@click.argument('fname', type=click.Path(exists=True))
+@click.option('-W', '--workdir', default="WORK", type=click.Path(exists=True), help="Work directory")
 @click.option('-l', '--label', default="hecss", help="Label for the calculations.")
 @click.option('-T', '--temp', default=300, type=float, help="Target temperature in Kelvin.")
+@click.option('-w', '--width', default=1.0, type=float, help="Initial scale of the prior distribution")
 @click.option('-C', '--calc', default="VASP", type=str,
               help="ASE calculator to be used for the job.\n" + "Supported calculators: VASP (default)")
 @click.option('--dfset/--no-dfest', default=True, help='Write DFSET file for ALAMODE')
 @click.option('-N', '--nsamples', default=10, type=int, help="Number of samples to be generated")
 @click.option('-c', '--command', default='./run-calc', help="Command to run calculator")
-def _hecss_sampler(fname, workdir, label, temp, calc, dfset, nsamples, command):
+def _hecss_sampler(fname, workdir, label, temp, width, calc, dfset, nsamples, command):
     '''
     Run HECSS sampler on the structure in the directory.
-    fname - Basic structure file. Any type recognized by ASE is accepted.
+    fname - Supercell structure file.
+    The containing directory must be readable by Vasp(restart).
     '''
     return hecss_sampler(fname=fname,
                          workdir=workdir,
                          label=label,
                          T=temp,
+                         width=width,
                          calc=calc,
                          dfset=dfset,
                          N=nsamples,
-                         command=command)
+                         cmd=command)
