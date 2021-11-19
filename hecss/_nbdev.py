7f8358d3714bef444f40c10fc0da48b2acfb6d18
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2021-11-19 20:04:18 +0100

    Reorganizing docs

--- hecss/_nbdev.py
+++ hecss/_nbdev.py
@@ -2,29 +2,29 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"normalize_conf": "02_core.ipynb",
-         "write_dfset": "02_core.ipynb",
-         "HECSS_Sampler": "02_core.ipynb",
-         "HECSS": "02_core.ipynb",
-         "dfset_writer": "03_cli.ipynb",
-         "hecss_sampler": "03_cli.ipynb",
-         "THz": "04_monitor.ipynb",
-         "plot_band_set": "04_monitor.ipynb",
-         "plot_bands": "04_monitor.ipynb",
-         "plot_bands_file": "04_monitor.ipynb",
-         "run_alamode": "04_monitor.ipynb",
-         "get_dfset_len": "04_monitor.ipynb",
-         "show_dc_conv": "04_monitor.ipynb",
-         "build_bnd_lst": "04_monitor.ipynb",
-         "build_omega": "04_monitor.ipynb",
-         "plot_omega": "04_monitor.ipynb",
-         "monitor_phonons": "04_monitor.ipynb",
-         "load_dfset": "04_monitor.ipynb",
-         "plot_stats": "04_monitor.ipynb",
-         "monitor_stats": "04_monitor.ipynb"}
+index = {"dfset_writer": "02_CLI.ipynb",
+         "hecss_sampler": "02_CLI.ipynb",
+         "normalize_conf": "11_core.ipynb",
+         "write_dfset": "11_core.ipynb",
+         "HECSS_Sampler": "11_core.ipynb",
+         "HECSS": "11_core.ipynb",
+         "THz": "12_monitor.ipynb",
+         "plot_band_set": "12_monitor.ipynb",
+         "plot_bands": "12_monitor.ipynb",
+         "plot_bands_file": "12_monitor.ipynb",
+         "run_alamode": "12_monitor.ipynb",
+         "get_dfset_len": "12_monitor.ipynb",
+         "show_dc_conv": "12_monitor.ipynb",
+         "build_bnd_lst": "12_monitor.ipynb",
+         "build_omega": "12_monitor.ipynb",
+         "plot_omega": "12_monitor.ipynb",
+         "monitor_phonons": "12_monitor.ipynb",
+         "load_dfset": "12_monitor.ipynb",
+         "plot_stats": "12_monitor.ipynb",
+         "monitor_stats": "12_monitor.ipynb"}
 
-modules = ["core.py",
-           "cli.py",
+modules = ["cli.py",
+           "core.py",
            "monitor.py"]
 
 doc_url = "https://jochym.gitlab.io//hecss/"
