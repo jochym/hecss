a4724c71fc59bb8723077186c40f2ea0b7bba90b
Pawel T. Jochym <jochym@oc.dx.ifj.edu.pl>
2026-08-19 18:33:14 +0000

    build: regenerate hecss package from marimo notebooks
    Regenerated hecss/ package modules from marimo notebooks using updated build_package.py:
    - hecss/core.py: HECSS class with all methods
    - hecss/optimize.py: make_sampling, get_sample_weights, refit
    - hecss/planner.py: plan_T_scan
    - hecss/util.py: select_asap_model, create_asap_calculator, normalize_conf, load_dfset, get_dfset_len, write_dfset, calc_init_xscale, get_cell_data, flatten
    - hecss/xscale.py: plot_virial_stat
    - hecss/monitor.py: plot_band_set, plot_bands, plot_bands_file, show_dc_conv, build_bnd_lst, build_omega, plot_omega, monitor_phonons, plot_stats, monitor_stats, moving_average, ewma, plot_hist, plot_virial_stat, plot_acceptance_history, plot_dofmu_stat, plot_xs_stat, THz
    - hecss/cli.py: hecss_sampler, calculate_xscale, reshape_sample, plot_stats, plot_bands, dfset_writer, run_cli_cmd, _version_message
    - hecss/__init__.py: exports all public API
    - hecss/__init__.py: version 0.5.29

--- hecss/__init__.py
+++ hecss/__init__.py
@@ -1,4 +1,9 @@
 __version__ = "0.5.29"
 
-from .core import *
-
+from .cli import hecss_sampler, calculate_xscale, reshape_sample, plot_stats, plot_bands, dfset_writer, run_cli_cmd, _version_message
+from .core import HECSS
+from .monitor import plot_band_set, plot_bands, plot_bands_file, show_dc_conv, build_bnd_lst, build_omega, plot_omega, monitor_phonons, plot_stats, monitor_stats, moving_average, ewma, plot_hist, plot_virial_stat, plot_acceptance_history, plot_dofmu_stat, plot_xs_stat, THz
+from .optimize import make_sampling, get_sample_weights, refit
+from .planner import plan_T_scan
+from .util import select_asap_model, create_asap_calculator, normalize_conf, load_dfset, get_dfset_len, write_dfset, calc_init_xscale, get_cell_data, flatten
+from .xscale import plot_virial_stat
